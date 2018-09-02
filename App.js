87cf72eb4417591e73941a333cef45177c089d57
nitin <25nitin.nagar@gmail.com>
2018-09-03 00:10:24 +0530

    add redux

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+
+import reducer from './reducer';
+import RepoList from './RepoList';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+  responseType: 'json'
+});
+
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to ope.</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+         <RepoList/>
+        </View>
+      </Provider>
     );
   }
 }
