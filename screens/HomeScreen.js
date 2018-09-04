d08aba685a271115e87d6588de0f513a53c4929a
nitin <25nitin.nagar@gmail.com>
2018-09-04 23:05:08 +0530

    tabs

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,42 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-
-export default class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
+import * as React from 'react';
+import { View,Text, StyleSheet, Dimensions } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+ 
+const FirstRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+);
+ 
+export default class HomeScreen extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+    ],
+  };
+ 
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Home Screen </Text>
-      </View>
+      <TabView
+        navigationState={this.state}
+        renderScene={SceneMap({
+          first: FirstRoute,
+          second: SecondRoute,
+        })}
+        onIndexChange={index => this.setState({ index })}
+        initialLayout={{ width: Dimensions.get('window').width }}
+      />
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
